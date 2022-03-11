da6c9f3ef013fbe26186df1d7b3cea2f0fc5323d
Denise R <jobs.1243236@gmail.com>
2022-03-11 16:08:06 -0300

    Create condicionais.js

---
+++ condicionais.js
@@ -0,0 +1,31 @@
+let verdade=false
+if(verdade){
+  console.log('true')
+}
+
+if(verdade){
+  console.log('true')
+}else{
+  console.log('false')
+}
+
+if(verdade){
+  console.log('true')
+}
+else if (verdade){
+  console.log('2')
+}
+
+let expression='b4'
+switch (expression){
+  case 'a':
+    console.log('1')
+    break
+  case 'b':
+    console.log('2')
+    break
+  default:
+    console.log('default')
+    break
+    
+}
